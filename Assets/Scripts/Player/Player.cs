4a76d3b781677a167524d5f67e6f98756b67b479
ViniciuslimaSiqueiras <soudelimasiqueira@gmail.com>
2024-04-16 21:41:46 -0300

    Jump implement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Vector2 velocity;
+    [Header("Walk")]
     public float speed;
+    public Vector2 friction = new Vector2(-.1f,0);
+
+    [Header("Jump")]
+    public float forceJump; 
 
     Rigidbody2D rb;
 
@@ -16,16 +20,36 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        
+        jump();
+        walk();
+    }
+
+    private void walk()
+    {
         if (Input.GetKey(KeyCode.A))
         {
             // rb.MovePosition(rb.position - velocity * Time.deltaTime);
             rb.velocity = new Vector2(-speed, rb.velocity.y);
-        }      
+        }
         if (Input.GetKey(KeyCode.D))
         {
-           // rb.MovePosition(rb.position + velocity * Time.deltaTime);
+            // rb.MovePosition(rb.position + velocity * Time.deltaTime);
             rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        if(rb.velocity.x > 0)
+        {
+            rb.velocity -= friction;
+        }else if(rb.velocity.x < 0)
+        {
+            rb.velocity += friction;
+        }
+
+    }
+    private void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * forceJump;
         }
     }
 }
